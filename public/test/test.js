cdf89daf8a9c9a4e4008ae742cba6f0b50a9f648
Julian <julian@Julians-MacBook-Pro.local>
2016-08-22 20:55:41 +0200

    using steal 0.16, steal-mocha and get testee running

--- public/test/test.js
+++ public/test/test.js
@@ -1,21 +1,26 @@
-import 'rss-job-tracker/models/fixtures/';
+// import 'rss-job-tracker/models/fixtures/';
+//
+// // import 'rss-job-tracker/models/test';
+// import 'rss-job-tracker/test/functional';
+//
+// import 'rss-job-tracker/components/welcome-message.component';
+// import 'rss-job-tracker/components/app-nav/app-nav_test';
+// import 'rss-job-tracker/components/tasks/tasks_test';
+// import 'rss-job-tracker/components/task-day/task-day_test';
+// import 'rss-job-tracker/components/job-reports/job-reports_test';
+// import 'rss-job-tracker/components/new-lot/new-lot_test';
+// import 'rss-job-tracker/components/new-task-day/new-task-day_test';
+// import 'rss-job-tracker/components/custom-work-order/custom-work-order_test';
+// import 'rss-job-tracker/components/dynamic-grid/dynamic-grid_test';
+// import 'rss-job-tracker/components/date-range-picker/date-range-picker_test';
+// import 'rss-job-tracker/components/new-task-day/tasks-completed/tasks-completed_test';
+// import 'rss-job-tracker/components/autocomplete/autocomplete_test';
+//
+// import 'rss-job-tracker/components/user-login/user-login_test';
+//
+// import 'rss-job-tracker/components/user-manager/user-manager_test';
 
-// import 'rss-job-tracker/models/test';
-import 'rss-job-tracker/test/functional';
 
-import 'rss-job-tracker/components/welcome-message.component';
-import 'rss-job-tracker/components/app-nav/app-nav_test';
-import 'rss-job-tracker/components/tasks/tasks_test';
-import 'rss-job-tracker/components/task-day/task-day_test';
-import 'rss-job-tracker/components/job-reports/job-reports_test';
-import 'rss-job-tracker/components/new-lot/new-lot_test';
-import 'rss-job-tracker/components/new-task-day/new-task-day_test';
-import 'rss-job-tracker/components/custom-work-order/custom-work-order_test';
-import 'rss-job-tracker/components/dynamic-grid/dynamic-grid_test';
-import 'rss-job-tracker/components/date-range-picker/date-range-picker_test';
-import 'rss-job-tracker/components/new-task-day/tasks-completed/tasks-completed_test';
-import 'rss-job-tracker/components/autocomplete/autocomplete_test';
+import "steal-mocha";
 
-import 'rss-job-tracker/components/user-login/user-login_test';
-
-import 'rss-job-tracker/components/user-manager/user-manager_test';
+import "rss-job-tracker/components/foo/test.js";
